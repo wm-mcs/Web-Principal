4eafc9a05221cb0eea0f058273181daee23d0ad0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-01-28 14:56:59 -0300

    df

--- resources/views/paginas/home/home_about.blade.php
+++ resources/views/paginas/home/home_about.blade.php
@@ -6,9 +6,10 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading text-white">¡En "WEBS" amamos el diseño!</h2>
+            <h2 class="section-heading text-white">¿Quieres tener una pagina web pero no tienes idea ni de como empesar?</h2>
+            <h2 class="section-heading text-white">¿Te gustaría contar con alguien que te ayude a crearla, diseñarla, subirla a internet y mantenerla?</h2>
             <hr class="light my-4">
-            <p class="text-white mb-4">Vamos a poner más el mas del 100% para que tu web sea vea linda y esté totalmente en sintonía con tu emprendimiento.
+            <p class="text-white mb-4">Aqui te vamos a poder asistir en todas esas etapas. Desde el diseño de la pagina, el cual se basara acorde a tus colores y logotipo; la puesta en marcha de hacer que todo quede online y funcionando, y tambien del mantenimiento (solucion a los incovenientes extraños que sucedan) para que peudas dormir tranquilo/a.
             </p>
             
           </div>
