c3f4bf85923dba9d6b4854e5171cf48942298d2c
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-06 14:40:28 -0300

    df

--- resources/views/admin/empresa/empresa_home.blade.php
+++ resources/views/admin/empresa/empresa_home.blade.php
@@ -1,22 +1,14 @@
 @extends('layouts.admin_layout.admin_layout')
 
+@section('miga-de-pan') 
+  <span>Mi Empresa</span>
+@stop
+
 @section('content')
 
-<div class="admin-contnedor-navegacion-miga">
-  {{-- home --}}
-  <a href="{{route('get_admin_home')}}"><span class="icon-home"></span></a>
 
-  {{-- separador --}}
-  <span class="spam-separador">|</span> 
 
-  {{-- lugar donde esta --}}
-  <span>Mi Empresa</span>
-</div>
 
-<div class="contenedor-admin-entidad">
-
- {{-- titulo --}}
- <div class="admin-entidad-titulo">Mi Empresa</div>
 
  <div class="get_width_100 Helper-OrdenarHijos-columna"> 
           {{-- formulario --}}
@@ -27,19 +19,19 @@
                                   ])               !!}
            <div class="formulario-contenedor">
               {{-- datos corporativos --}}
-              {{-- <div class="contenedor-grupo-datos">
+              <div class="contenedor-grupo-datos">
                 <div class="contenedor-grupo-datos-titulo"> Identidad</div>
                 <div class="contenedor-formulario-label-fiel">                       
                   @include('admin.empresa.formularios_partes.datos_basicos')
                 </div>
-              </div> --}}
+              </div>
               {{-- imagenes corporativos --}}
-              {{-- <div class="contenedor-grupo-datos">
+              <div class="contenedor-grupo-datos">
                 <div class="contenedor-grupo-datos-titulo"> Imagen Corporativa</div>
                 <div class="contenedor-formulario-label-fiel">                       
                   @include('admin.empresa.formularios_partes.datos_imagenes')
                 </div>
-              </div> --}}
+              </div>
               {{-- datos de contacto --}}
               <div class="contenedor-grupo-datos">
                 <div class="contenedor-grupo-datos-titulo"> Contacto</div>
