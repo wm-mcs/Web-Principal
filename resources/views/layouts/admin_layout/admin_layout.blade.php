df745819d96acf1b49a22dfec2f0a7b451859714
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-06 15:52:34 -0300

    fg

--- resources/views/layouts/admin_layout/admin_layout.blade.php
+++ resources/views/layouts/admin_layout/admin_layout.blade.php
@@ -19,6 +19,7 @@
   
    <div class="admin-contiene-columna-y-content">
       @include('layouts.admin_layout.columna_derecha.columna')
+      <div class="get_width_20"></div>
       <div class="admin-contiene-content">
         
           <div class="admin-contiene-content-wraper"> 
