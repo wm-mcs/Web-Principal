6ec05df0c269587127f0f36f796759c73512cf48
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-06 13:41:34 -0300

    df

--- resources/views/layouts/admin_layout/admin_layout.blade.php
+++ resources/views/layouts/admin_layout/admin_layout.blade.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Administrador Panel</title>
-    <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/admin.css') }}">   
-    <link rel="stylesheet" type="text/css" href=" {{ asset('Iconos/fonts/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/admin.css') }}">  
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"> 
+    
     <META name="robots" content="NOINDEX,NOFOLLOW">
   </head>
 
--- resources/views/layouts/admin_layout/columna_derecha/columna.blade.php
+++ resources/views/layouts/admin_layout/columna_derecha/columna.blade.php
@@ -7,7 +7,7 @@
    @if(Auth::user()->role === 'adminMcos522')
    <div id="admin-col-superadmin">
         <a href="{{route('get_datos_home_web')}}">
-            <li class="admin-columna-li mi-float-right"><span class="glyphicon glyphicon-triangle-right"></span> Admin Panel</li>
+            <li class="admin-columna-li mi-float-right"><i class="fas fa-igloo"></i> Admin Panel</li>
         </a> 
         <a href="{{route('get_admin_users')}}">
           <li class="admin-columna-li mi-float-right"><span class="glyphicon glyphicon-triangle-right"></span> Usuarios</li>
