9a2d4e3a9b22f27652f7eeead2aee02fbb2a1229
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-12 18:42:43 -0300

    fg

--- resources/assets/js/Customs/mis-file_input.js
+++ resources/assets/js/Customs/mis-file_input.js
@@ -1,5 +1,4 @@
 // field para subir imagenes de eventos en crear y editar
 
 
-$("#imagenes-eventos-field").fileinput({ language:"es"});
-$("#imagenes-edicion").fileinput({ language:"es"});
+$("#imagenes-field").fileinput({ language:"es"});
--- resources/views/admin/productos/formularios_partes/datos_imagenes.blade.php
+++ resources/views/admin/productos/formularios_partes/datos_imagenes.blade.php
@@ -2,7 +2,7 @@
 <div class="formulario-label-fiel">
 {!! Form::label('img', 'Imagen', array('class' => 'control-label')) !!}
 {!! Form::file('img[]',['class'            => 'file',
-                       'id'                => 'imagenes-eventos-field',
+                       'id'                => 'imagenes-field',
                        'multiple'          => true,
                        'data-show-upload'  =>'false',
                        'data-show-caption' => 'true' 
