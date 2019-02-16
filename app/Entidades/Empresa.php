ff775890fb371b0a025abc42e6de7fa550264282
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-16 12:54:33 -0300

    fg

--- app/Entidades/Empresa.php
+++ app/Entidades/Empresa.php
@@ -141,6 +141,8 @@ class Empresa extends Model
         $numero  = '598'. substr(trim($this->whatsapp_empresa),1);
         $mensaje = 'Hola!%20,%20quiero%20consultar%20por%20los%20planes%20de%20webs%20que%20tienen!';
         $url = 'https://api.whatsapp.com/send?phone='. $numero .'&text='. $mensaje;
+
+        return $url;
     }
 
 
--- resources/views/paginas/home/home_footer.blade.php
+++ resources/views/paginas/home/home_footer.blade.php
@@ -51,7 +51,7 @@
 
                   @if($Empresa->whatsapp_empresa != 'no')
                   <a class="footer-icono-social" href="{{$Empresa->link_whatsapp_send}}"> 
-                    <i class="fa fa-whatsapp"></i>
+                    <i class="fa whatsapp-square"></i>
                   </a>
                  @endif
 
