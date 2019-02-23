bf52dbaf0050ef257cbd50504fb2ceacd63a9623
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-22 21:58:48 -0300

    ok

--- resources/views/paginas/noticias/noticia_individual.blade.php
+++ resources/views/paginas/noticias/noticia_individual.blade.php
@@ -31,6 +31,38 @@
 
 @section('content')
 
+<div  class="{{-- masthead --}} get_width_100 "  >
+  
+<div id="carouselExampleIndicators" class="carousel slide auto" data-ride="carousel" data-interval="5000">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="img-slider-size Slider_cabecer_img" src="{{url()}}/imagenes/computacion-en-la-niebla-la-alternativa-a-la-nube-1920.jpg"> 
+     
+    </div>
+   
+  </div>
+
+
+      <div class="carousel-caption  Slider-Contendor d-md-block Helper-OrdenarHijos-Row">
+        <div class="get_width_100 flex-row-center" style="min-height: 100vh;">
+          <div class="get_width_100">
+              <div class="col-lg-10 mx-auto">
+                <h1 class="text-uppercase Slider_cabecera_caption_titulo text-color-black">
+                  <strong>{Titulo del post}</strong>
+                </h1>
+               </div>  
+          </div>
+        </div>
+      </div>
+      {{-- <img src="{{url()}}/imagenes/580b57fcd9996e24bc43c513.png" class="Slider_cabecera_caption_logo">   --}}
+  
+
+</div>
+
   <div class="contenedor-listado-noticias">
     
     @include('paginas.noticias.noticias_lista_individual')
