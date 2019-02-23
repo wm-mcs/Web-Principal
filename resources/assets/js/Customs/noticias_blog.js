fc533bba815cc7a4b74e730ee4e9a48bb24ddbee
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-23 20:29:48 -0300

    fg

--- resources/assets/js/Customs/noticias_blog.js
+++ resources/assets/js/Customs/noticias_blog.js
@@ -1,7 +1,18 @@
+  
+
+ /* Aqui creo esta funcion para cambiar a entidades 
+  de html el contenido del blog que viene de la base 
+  de datos con html pero en texto plano*/
+
   var contenedor_noticia = $( "#contenido-noticia" ),
   str = contenedor_noticia.text(),
   html = $.parseHTML( str ),
   nodeNames = [];
  
-  // Append the parsed HTML
+  
+
+  //borro el contenido
+  contenedor_noticia.text( '' );
+
+  //agrego el contenido
   contenedor_noticia.append( html );
