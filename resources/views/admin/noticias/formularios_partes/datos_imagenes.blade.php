58a826b2a347184d30f0646b3173d7ed451f0d99
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-02-26 09:13:25 -0300

    sd

--- resources/views/admin/noticias/formularios_partes/datos_imagenes.blade.php
+++ resources/views/admin/noticias/formularios_partes/datos_imagenes.blade.php
@@ -7,6 +7,11 @@
 <div class="formulario-label-fiel">
 {!! Form::label('img2', 'Imagen Secundaria', array('class' => 'formulario-label ')) !!}
 {!! Form::file('img2',['class' => 'formulario-field']) !!}   
+</div> 
+
+<div class="flex-row-column get_width_100" >
+  <img class="admin-img-section-img" src="{{$Entidad->url_img_portada}}">
+  <img class="admin-img-section-img" src="{{$Entidad->url_img_adicional}}">  
 </div>
 
 
