7bfcef942715feb26ea1fbd2b5fce7c0e80fa977
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-03-13 17:34:53 -0300

    fg

--- app/Repositorios/ProductoImgRepo.php
+++ app/Repositorios/ProductoImgRepo.php
@@ -22,7 +22,7 @@ class ProductoImgRepo extends BaseRepo
   {
     $imagen = $this->find($id_img);
 
-    $imagen_pricipal = $this->get_imagen_principal_de_entidad_especifica('producto_id',$imagen->evento_id) ;  
+    $imagen_pricipal = $this->get_imagen_principal_de_entidad_especifica('producto_id',$imagen->producto_id) ;  
 
     $this->cambio_a_imagen_principal_desde_base_repo($imagen_pricipal,$imagen);
   }
