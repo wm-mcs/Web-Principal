4e85f0f91f238f745c735802ccd72b1b0637a865
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-04-22 16:26:39 -0300

    fg

--- resources/views/paginas/home/home_tener_pagina_web.blade.php
+++ resources/views/paginas/home/home_tener_pagina_web.blade.php
@@ -5,7 +5,7 @@
             <h2 class="section-heading">¿Por qué tener una página web?</h2>
             <hr class="my-4">
             <h4 class="text-muted mb-4">Te dejo 5 razones..</h4>
-             <p class="mb-4">Aqui te vamos a poder asistir en todas esas etapas. Desde el diseño de la pagina, el cual se basara acorde a tus colores y logotipo; la puesta en marcha de hacer que todo quede online y funcionando, y tambien del mantenimiento (solucion a los incovenientes extraños que sucedan) para que peudas dormir tranquilo/a.
+             <p class="mb-4"> 1) Visibilidad:  La primera razón es la visibilidad que la página web le aporta a tu negocio, te permite estar disponible las 24 horas del día en favor de tu target. Cuando alguien escucha acerca de un negocio, lo primero que hace es buscarlo en internet. Si no estás presente en internet, probablemente perderás un potencial cliente.
             </p>
           </div>
         </div>
