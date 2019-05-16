2a8bb0e3ddf211e4a0d346550a040ddad0e40a21
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-05-16 14:26:48 -0300

    df

--- resources/views/layouts/gestion_socios_layout/columna_derecha/columna.blade.php
+++ resources/views/layouts/gestion_socios_layout/columna_derecha/columna.blade.php
@@ -1,7 +1,7 @@
 <div class="admin-columna-contenedor">
 
  {{-- imagen logo --}}
- <a href="{{route('get_home')}}"><img class="admin-header-logo" src="{{$Empresa->url_img}}"></a>
+ <a href="{{route('get_home')}}"><img class="admin-header-logo" src="{{$Empresa_gestion->url_img}}"></a>
 
  <ul>
    @if(Auth::user()->role === 'adminMcos522')
