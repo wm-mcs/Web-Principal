6cc04f2017d8861fcf47a06b0f83cf412f086074
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-05-30 17:02:32 -0300

    fg

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socios-crear-boton_componente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socios-crear-boton_componente.blade.php
@@ -22,8 +22,8 @@ methods:{
 
 
 },
-template:'<span>
-   <div id="socio-boton-crear" class="admin-user-boton-Crear" v-on:click="abrir_modal">
+template:'<span >
+   <div id="socio-boton-crear" style="position:relative;" class="admin-user-boton-Crear" v-on:click="abrir_modal">
         @{{ accion_name }} socio <i class="fas fa-user-plus"></i>
 
 
