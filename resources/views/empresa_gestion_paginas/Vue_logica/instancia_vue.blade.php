466f67a402f97f8d4a58d554f3a294b0af00b01f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-06-06 17:42:25 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/instancia_vue.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/instancia_vue.blade.php
@@ -13,32 +13,31 @@
       
 
     },
-    mounted:{  
-      
-      this.getSociosActivos();
-
-    },
-
     methods:{
 
      getSociosActivos:fucntion()
      {
+
+     }
+
+    },
+
+    mounted: function () {        
+
        var url = '/get_socios_activos';
 
-       axios.get(url).then(function(response){  
-
-           this.socios = response.socios;
+       axios.get(url).then(function (response){  
+           this.socios = response.data.socios;
            console.log(response.data.socios);
            
            }).catch(function (error){
 
                      
             
-       });
-     }
+           });
 
-    },
-    
+
+    }
 
    });
 
