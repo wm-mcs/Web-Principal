67d50a2081d064968c4f1f8b1144fde9e016983c
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-06-08 12:50:36 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-componente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-entidad-listado-componente.blade.php
@@ -19,7 +19,20 @@ template:'<span>
 
   <div v-if="socios.length > 0" class="listado-socios-contenedor-lista">
     <div v-for="socio in socios" :key="socio.id" class="listado-socios-contenedor-individual">
-      @{{socio.name}}      
+     
+      <div class="listado-socios-sub-contenedor-name-estado">
+        <div class="listado-socios-name">@{{socio.name}}</div>
+        <div>@{{socio.estado}}</div>
+
+
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+          
     </div>
   </div> 
 
