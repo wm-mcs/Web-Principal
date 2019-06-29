b91712dbb180cd40a8fc2f2cafc20a42471edabd
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-06-29 18:04:03 -0300

    fg

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/agregar-al-socio-un-servicio-componente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/agregar-al-socio-un-servicio-componente.blade.php
@@ -39,7 +39,7 @@ template:'<span>
        
  </div>
 
-    <div class="modal fade" id="modal-editar-socio" tabindex="+1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal fade" id="modal-agregar-servicio-socio" tabindex="+1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
