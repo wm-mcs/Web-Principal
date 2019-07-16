21ec656e86634556b385c0545cf3510b62f239f2
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-07-16 16:03:36 -0300

    hj

--- resources/views/paginas/home/home_pasos_para_concretar.blade.php
+++ resources/views/paginas/home/home_pasos_para_concretar.blade.php
@@ -6,10 +6,11 @@
             <hr class="light my-4">
             <p class="text-white mb-4">Elegí el plan que mas te guste y luego contactame por el mismo. Basicamente te vamos a pedir algunas cosas como tu logotipo de la empresa o emprendimiento e información para saber de que trata. Con esos datos elaboraremos una muestra y te la enviaremos para que nos confirmes. Luego de que este todo ok, 
             </p>
+            
             <a href="" class="get_width_100 js-scroll-trigger" href="#precios">
-            <div class="get_width_100 flex-row-center flex-justifice-space-around mt-5">
-                <span class="boton-simple-blanco  " > Ver precios </span>
-            </div>
+              <div class="get_width_100 flex-row-center flex-justifice-space-around mt-5">
+                  <span class="boton-simple-blanco  " > Ver precios </span>
+              </div>
             </a>
 
 
