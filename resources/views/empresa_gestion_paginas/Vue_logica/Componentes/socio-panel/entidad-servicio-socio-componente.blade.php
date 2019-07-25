5304277a5a0285883a8653a8f64cb679377ec7ab
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-07-25 09:23:01 -0300

    df

--- resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/entidad-servicio-socio-componente.blade.php
+++ resources/views/empresa_gestion_paginas/Vue_logica/Componentes/socio-panel/entidad-servicio-socio-componente.blade.php
@@ -95,7 +95,7 @@ computed:{
 
     esta_activo:function()
     {
-        if(this.servicio.esta_vencido == true || this.servicio.esta_consumido == false)
+        if( (this.servicio.esta_vencido == true) || (this.servicio.esta_consumido == false) )
         {
           return false ;
         }
