cd9349125f185bfba72b0e99cfbf06946331f872
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-08-06 17:15:30 -0300

    df

--- resources/views/paginas/home/home_about.blade.php
+++ resources/views/paginas/home/home_about.blade.php
@@ -9,7 +9,7 @@
             <h2 class="section-heading text-white">¿Quieres tener una pagina web pero no tienes idea ni de como empesar?</h2>
             <h2 class="section-heading text-white">¿Te gustaría contar con alguien que te ayude a crearla, diseñarla, subirla a internet y mantenerla?</h2>
             <hr class="light my-4">
-            <p class="text-white mb-4">Aqui te vamos a poder asistir en todas esas etapas. Desde el diseño de la pagina, el cual se basara acorde a tus colores y logotipo; la puesta en marcha de hacer que todo quede online y funcionando, y tambien del mantenimiento (solucion a los incovenientes extraños que sucedan) para que peudas dormir tranquilo/a.
+            <p class="text-white mb-4">Aquí te vamos a poder asistir en todas esas etapas. Desde el diseño de la página, el cual se basara acorde a tus colores y logotipo; la puesta en marcha de hacer que todo quede online y funcionando, y también del mantenimiento (solución a los inconvenientes extraños que sucedan) para que puedas dormir tranquilo/a.
             </p>
             
           </div>
--- resources/views/paginas/home/slider_texto_present.blade.php
+++ resources/views/paginas/home/slider_texto_present.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="col-lg-8 mx-auto">
-  <h2 class="text-faded mb-5 Slider_cabecera_caption_sub_titulo text-color-primary">Tenemos la que necesitas.</h2>
+  <h2 class="text-faded mb-5 Slider_cabecera_caption_sub_titulo text-color-primary">Tengo la que necesitas.</h2>
  
   <a class="btn btn-primary btn-xl js-scroll-trigger Slider_cabecera_boton_contacto" href="#precios">Ver planes</a>
    <p class="text-faded my-4 text-color-primary" style="font-style:italic; font-weight: bold;">#WEBSDECALIDAD</p>
